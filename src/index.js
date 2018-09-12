d4a3d3d34dc10f48f69850ead377dcc9c534ec3c
803142 <803142@gmail.com>
2018-09-13 00:51:02 +0300

    exclude repetitions in calculations

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  count=0;
+  lnth=preferences.length;
+  //triangle can be only with three person
+  if (lnth>=3){
+  for (i=0;i<=lnth-3;i++)  {
+  		a=preferences[i]-1;
+  		b=preferences[a]-1;
+  		c=preferences[b]-1;
+  		//exclude repetitions in calculations
+  		if ((c == i) && (b>i) && (a>i)){count=count+1;
+  	 	};
+	};
+
+  };
+
+  return count;
+  };
+	
